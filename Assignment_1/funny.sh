091aa1e245423e0fc348d3943bdcac9ccc89b53e
Sahill Sharma <53467580+sahill95@users.noreply.github.com>
2021-03-19 07:36:16 +1100

    Delete Assignment_1 directory

--- Assignment_1/funny.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script will print out 'This is funny' if an argument is included, otherwise will print 'This is NOT funny'
-if [ -z "$1" ]; then
-        echo "This is NOT funny"
-else
-        echo "This is funny"
-fi
